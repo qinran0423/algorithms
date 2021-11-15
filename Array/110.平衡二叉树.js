52d30dcf20b05356ba27279e67b05ea114d477b6
qinran0423 <1156107187@qq.com>
2021-11-15 23:01:24 +0800

    leetcode 110

---
+++ Array/110.平衡二叉树.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=110 lang=javascript
+ *
+ * [110] 平衡二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+
+  return getHeight(root) >= 0
+
+};
+
+
+var getHeight = function (root) {
+  if (!root) return 0
+  let l = getHeight(root.left)
+  let r = getHeight(root.right)
+  if (l < 0 || r < 0) return -1;
+  if (Math.abs(l - r) > 1) return -1;
+  return Math.max(l, r) + 1
+
+}
+// @lc code=end
+
