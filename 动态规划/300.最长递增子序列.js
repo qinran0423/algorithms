9d96be5736ae4b6228791c5803a88d8e9ef9cd9b
qinran0423 <1156107187@qq.com>
2022-05-21 22:24:22 +0800

    70 300

---
+++ 动态规划/300.最长递增子序列.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  let n = nums.length
+  if (n === 0) {
+    return 0
+  }
+  let dp = Array(n).fill(1)
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1)
+      }
+    }
+  }
+
+  return Math.max(...dp)
+};
+// @lc code=end
+
--- 动态规划/70.爬楼梯.js
+++ 动态规划/70.爬楼梯.js
@@ -10,13 +10,14 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  let dp = []
-  dp[0] = 1
-  dp[1] = 1
-  for (let i = 2; i <= n; i++) {
+
+  let dp = [1, 2]
+
+  for (let i = 2; i < n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2]
   }
-  return dp[n]
+
+  return dp[n - 1]
 };
 // @lc code=end
 
