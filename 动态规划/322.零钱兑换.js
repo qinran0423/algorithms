20f0dd41b13fa1f27902e2b05a70db318cf6babd
qinran0423 <1156107187@qq.com>
2022-06-04 15:03:42 +0800

    322

--- 动态规划/322.零钱兑换.js
+++ 动态规划/322.零钱兑换.js
@@ -1,5 +1,3 @@
-
-
 /*
  * @lc app=leetcode.cn id=322 lang=javascript
  *
@@ -12,17 +10,19 @@
  * @param {number} amount
  * @return {number}
  */
+// dp[i]：当目标金额为i时， 至少需要 dp[i]枚硬币凑出
 var coinChange = function (coins, amount) {
-
-  if (!amount) {
-    return 0
-  }
-
   let dp = new Array(amount + 1).fill(Infinity)
   dp[0] = 0
-  for (let i = 0; i < coins.length; i++) {
-    for (let j = coins[i]; j <= amount; j++) {
-      dp[j] = Math.min(dp[j - coins[i]] + 1, dp[j])
+  for (let i = 1; i <= amount; i++) {
+    for (const coin of coins) {
+      if (i - coin >= 0) {
+        // 比如 11 =》 10 + 1  10的最少硬币数 + 1
+        //        =》 9 + 2   9的最少硬币数 + 1
+        //        =》 6 + 5   6的最少硬币数 + 1
+
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1)
+      }
     }
   }
   return dp[amount] === Infinity ? -1 : dp[amount]
