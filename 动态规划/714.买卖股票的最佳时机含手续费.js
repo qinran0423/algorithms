d1ca56682ec8499ca264ed5b7403de567441334d
qinran0423 <1156107187@qq.com>
2022-06-09 11:19:12 +0800

    714

--- 动态规划/714.买卖股票的最佳时机含手续费.js
+++ 动态规划/714.买卖股票的最佳时机含手续费.js
@@ -49,5 +49,27 @@ var maxProfit1 = function (prices, fee) {
 
 
 };
+
+
+
+
+// 动态规划
+var maxProfit2 = function (prices, fee) {
+  let n = prices.length
+  let dp = new Array(n).fill(0).map(() => {
+    return new Array(2).fill(0)
+  })
+  dp[0][0] = 0
+  dp[0][1] = -prices[0]
+
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+  }
+
+
+  return Math.max(dp[n - 1][0], dp[n - 1][1])
+}
+
 // @lc code=end
 
