679e7530045dffe926f6a975f5c4b811371371c8
qinran0423 <1156107187@qq.com>
2022-06-28 21:55:01 +0800

    347

---
+++ Heap/347.前-k-个高频元素.js
@@ -0,0 +1,139 @@
+/*
+ * @lc app=leetcode.cn id=347 lang=javascript
+ *
+ * [347] 前 K 个高频元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+class Heap {
+  constructor(compareFn) {
+    this.compareFn = compareFn
+    this.heap = []
+  }
+
+  insert(value) {
+    // 向堆中插入值： 将值插入堆的底部叶节点在执行siftUp
+    if (value !== null) {
+      this.heap.push(value)
+      this.siftUp(this.heap.length - 1)
+      return true
+    }
+    return false
+  }
+
+  siftUp(index) {
+    let parent = this.getParentIndex(index)
+    while (index > 0 && this.compareFn(this.heap[index], this.heap[parent])) {
+      this.swap(this.heap, parent, index)
+      index = parent
+      parent = this.getParentIndex(index)
+    }
+  }
+
+  siftDown(index) {
+    while (this.getLeftIndex(index) < this.size()) {
+      let element = this.getLeftIndex(index)
+      if (this.getRightIndex(index) < this.size() && this.compareFn(this.heap[this.getRightIndex(index)], this.heap[this.getLeftIndex(index)])) {
+        element = this.getRightIndex(index)
+      }
+
+      if (this.compareFn(this.heap[index], this.heap[element])) {
+        return
+      }
+
+      this.swap(this.heap, index, element)
+      index = element
+    }
+  }
+
+  swap(arr, a, b) {
+    [arr[a], arr[b]] = [arr[b], arr[a]]
+  }
+
+  size() {
+    return this.heap.length
+  }
+
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  pop() {
+    // 弹出堆顶元素
+    this.swap(this.heap, 0, this.heap.length - 1)
+    let head = this.heap.pop()
+    this.siftDown(0)
+
+    return head
+  }
+
+  peek() {
+    // 获取堆顶元素
+    return this.isEmpty() ? undefined : this.heap[0]
+  }
+
+  getLeftIndex(index) {
+    return 2 * index + 1
+  }
+
+  getRightIndex(index) {
+    return 2 * index + 2
+  }
+
+  getParentIndex(index) {
+    if (index === 0) {
+      return undefined
+    }
+
+    return Math.floor((index - 1) / 2)
+  }
+}
+
+
+var topKFrequent = function (nums, k) {
+  let map = {}
+  for (const num of nums) {
+    if (num in map) {
+      map[num] += 1
+    } else {
+      map[num] = 1
+    }
+  }
+  // 维护一个大小为k的小顶堆
+  let heap = new Heap((a, b) => map[a] < map[b])
+
+  Object.keys(map).forEach((n, i) => {
+    if (i < k) {
+      heap.insert(n)
+    } else if (map[heap.peek()] < map[n]) {
+      heap.heap[0] = n
+      heap.siftDown(0)
+    }
+  })
+
+  return heap.heap
+
+
+}
+
+
+
+
+var topKFrequent1 = function (nums, k) {
+  let map = {}
+  for (const num of nums) {
+    if (num in map) {
+      map[num] += 1
+    } else {
+      map[num] = 1
+    }
+  }
+
+  return Object.keys(map).sort((a, b) => map[b] - map[a]).slice(0, k)
+};
+// @lc code=end
